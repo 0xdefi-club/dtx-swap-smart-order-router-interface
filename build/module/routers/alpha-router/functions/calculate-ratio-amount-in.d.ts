4a4e5816339aca3431ceed443a401e39148b5963
zj kkk <npckei217@gmail.com>
2024-06-27 20:11:20 +0800

    feat:update

---
+++ build/module/routers/alpha-router/functions/calculate-ratio-amount-in.d.ts
@@ -0,0 +1,8 @@
+import { Fraction } from '@uniswap/sdk-core';
+import { CurrencyAmount } from '../../../util/amounts';
+export declare function calculateRatioAmountIn(
+  optimalRatio: Fraction,
+  inputTokenPrice: Fraction,
+  inputBalance: CurrencyAmount,
+  outputBalance: CurrencyAmount
+): CurrencyAmount;
